8f79c39816946725fa56ef3d89c4b470c4e7e169
LadoSow <ladosow@gmail.com>
2021-06-21 17:20:36 +0000

    Digidex new version

--- src/config/Host.ts
+++ src/config/Host.ts
@@ -1,6 +1,6 @@
-const SERVER = 'http://127.0.0.1:8000';
+const SERVER = 'http://10.137.19.241:8095';
 /*
-const LOCAL = 'http://10.137.19.241:8095';
+const LOCAL = 'http://127.0.0.1:8000';
 */
 
 export const HOST = SERVER;
